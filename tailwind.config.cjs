d482a44afeb58bec7809b7f5455dd67d01310378
Gabriel Basaglia <garbaroots@hotmail.com>
2023-09-17 20:10:04 -0300

    Inserido nav e Footer

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF2E63',
+        secondary: '#08D906',
+        dark: '#252A34',
+        light: '#EAEAEA',
+      },
+    },
     fontFamily: {
       sans: ['Montserrat', 'ui-sans-serif', 'system-ui'],
     },
